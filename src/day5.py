56addeccb312c311b917d8b17b5b5c1e79af2f9e
jpribeir <joaop.ribeiro@outlook.pt>
2023-12-05 22:52:18 +0000

    Day 5 part 2 finished

--- src/day5.py
+++ src/day5.py
@@ -12,26 +12,32 @@ def getNewNums(seed_list,conversion_list):
 def getNewRanges(seed_range_list,conversion_dict):
     new_list = []
     rest_list = []
-    for seed in seed_range_list:
-        for key in conversion_dict:
-            print(key)
-            print(seed)
-            if seed[0]<key[0] and seed[1]<key[1]:
-                new_list.append((conversion_dict[key],conversion_dict[key]+seed[1]-key[0]))
-                rest_list.append((seed[0],key[0]))
-            elif seed[0]>key[0] and seed[1]>key[1]:
-                new_list.append((conversion_dict[key],conversion_dict[key]+key[1]-seed[0]))
-                rest_list.append((seed[1],key[1]))
-            elif seed[0]<key[0] and seed[1]>key[1]:
-                new_list.append((conversion_dict[key],conversion_dict[key]+key[1]-key[0]))
-                rest_list.append((seed[0],key[0]))
-                rest_list.append((key[1],seed[1]))
-            elif seed[0]>key[0] and seed[1]<key[1]:
-                new_list.append((conversion_dict[key],conversion_dict[key]+seed[1]-seed[0]))
-    return new_list+rest_list
+    for key in conversion_dict:
+        for seed in seed_range_list:
+            if seed[0]<key[0]:
+                if seed[1]<key[0]:
+                    rest_list.append(seed)
+                elif seed[1]<=key[1]:
+                    new_list.append((conversion_dict[key],conversion_dict[key]+seed[1]-key[0]))
+                    rest_list.append((seed[0],key[0]))
+                elif seed[1]>key[1]:
+                    new_list.append((conversion_dict[key],conversion_dict[key]+key[1]-key[0]))
+                    rest_list.append((seed[0],key[0]))
+                    rest_list.append((key[1],seed[1]))
+            elif seed[0]>=key[0]:
+                if seed[0]>key[1]:
+                    rest_list.append(seed)
+                elif seed[1]>key[1]:
+                    new_list.append((conversion_dict[key]+seed[0]-key[0],conversion_dict[key]+key[1]-key[0]))
+                    rest_list.append((key[1],seed[1]))
+                elif seed[1]<=key[1]:
+                    new_list.append((conversion_dict[key]+seed[0]-key[0],conversion_dict[key]+seed[1]-key[0]))
+        seed_range_list = list(set(rest_list.copy()))
+        rest_list = []
+    return new_list+seed_range_list
 
 # Read input file
-with open("../include/example5.inc","r") as almanac_file:
+with open("../include/input5.inc","r") as almanac_file:
     almanac_list = list(map(lambda a: a.strip(),almanac_file.readlines()))
 
 ########################### Part 1 ###########################
@@ -53,9 +59,7 @@ for line in almanac_list:
         for i in range(0,len(seed_list),2): seed_range_list.append((int(seed_list[i]),int(seed_list[i])+int(seed_list[i+1])))
     elif "-to-" in line: conversion_dict = {}
     elif line == "":
-        print(seed_range_list)
-        seed_range_list = getNewRanges(seed_range_list,conversion_dict)
+        if conversion_dict: seed_range_list = getNewRanges(seed_range_list,conversion_dict)
     else: conversion_dict[(int(line.split(" ")[1]),int(line.split(" ")[1])+int(line.split(" ")[2]))] = int(line.split(" ")[0])
 end_list = getNewRanges(seed_range_list,conversion_dict)
-print(end_list)
-print("Part2: %s"%min(seed_range_list))
+print("Part2: %s"%min(list(map(lambda a: a[0],end_list))))
