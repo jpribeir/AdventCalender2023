3a0198b8b05ad34645645b60df9e78c5db0c4e38
Joao Pedro Ribeiro <jpribeir@synopsys.com>
2023-12-22 18:25:48 +0000

    Day 20 part 1 still WIP

--- src/day20.py
+++ src/day20.py
@@ -1,23 +1,16 @@
 # Day 20 of the 2023 Advent of Code
-class FlipFlop():
-    def __init__(self,id,outsig):
+class Device():
+    def __init__(self,type,id,outsig):
+        self.type = type
         self.id = id
         self.outsig = outsig
-        self.status = 0
-    def recievePulse(type):
-        if type == "low":
-            self.status = 1 - self.status
-            return self.status
-
-class Conjunction():
-    def __init__(self,id,outsig):
-        self.id = id
-        self.memory = {}
-        self.outsig = outsig
-    def recievePulse(type,insig):
-        self.memory[insig] = type
-        if all(self.memory): return 0
-        else: return 1
+        if type == "%": self.status = 0
+        elif type == "&": self.memory = {}
+    def recievePulse(val):
+        if self.type == "%":
+            if type == "low": self.status = 1 - self.status
+        elif self.type == "&":
+            
 
 def pushButton(num_push):
     for push in range(num_push):
@@ -28,15 +21,11 @@ with open("../include/example20.inc","r") as connection_file:
     connection_list = connection_file.read().split("\n")
 
 ########################### Part 1 ###########################
-flipflop_dict = {}
-conjunction_dict = {}
+device_dict = {}
 instruction_order = []
 for net in connection_list:
     [input,output] = net.split(" -> ")
-    prefix = input[0]
-    id = input[1:]
-    if prefix == "%": flipflop_dict[id] = FlipFlop(id,output)
-    elif prefix == "&": conjunction_dict[id] = Conjunction(id,output)
-    elif prefix == "b": broadcaster_list = output
-    instruction_order.append(id)
+    prefix,id = input[0],input[1:]
+    device_dict[id] = Device(prefix,id,output)
+    instruction_order.append(input)
 print("Part 1: %s"%pushButton(1000))
