ba8908bcbdf771fc0a6f1c65cb674f2da2ebe4bb
jpribeir <joaop.ribeiro@outlook.pt>
2023-12-24 17:08:22 +0000

    Day 20 part 2 WIP

--- src/day20.py
+++ src/day20.py
@@ -6,7 +6,6 @@ class Device():
         self.outs = outs
         if type == "%": self.status = 0
         elif type == "&": self.memory = {}
-
     def recievePulse(self,origin,pulse):
         if self.type == "%":
             if pulse == 0:
@@ -34,7 +33,7 @@ def createDevices():
             if device_dict[each].type == "&": device_dict[each].memory[id] = 0
     return device_dict
 
-def pushButton(high_count,low_count):
+def pushButton(high_count,low_count,dev):
     instruction_queue.append(("utton",0))
     while instruction_queue:
         origin,pulse = instruction_queue.pop(0)
@@ -42,24 +41,23 @@ def pushButton(high_count,low_count):
             device_dict[out].recievePulse(origin,pulse)
             if pulse == 1: high_count += 1
             elif pulse == 0: low_count += 1
-            if out == "rx":
-                rx_in = pulse
-                print("rx: ",pulse)
-    return high_count,low_count,rx_in
+            if dev and out == dev:
+                if pulse == 1: return True
+    return high_count,low_count
 
 def pushSequence(num_push):
     high_count,low_count = 0,0
-    if num_push:
-        for _ in range(num_push): high_count,low_count,_ = pushButton(high_count,low_count)
+    if isinstance(num_push,int):
+        for _ in range(num_push): high_count,low_count = pushButton(high_count,low_count,None)
         return high_count*low_count
     else:
-        rxLow = False
+        foundDev = False
         push_count = 0
-        while not rxLow:
-            rx_in,_,_ = pushButton(0,0)
+        while not foundDev:
+            if pushButton(0,0,num_push): foundDev = True
             push_count += 1
-            if rx_in == 0: rxLow = True
-        return rx_in
+        print(push_count)
+        return push_count
 
 # Read input file
 with open("../include/input20.inc","r") as connection_file:
@@ -69,4 +67,9 @@ with open("../include/input20.inc","r") as connection_file:
 device_dict = createDevices()
 instruction_queue = []
 print("Part 1: %s"%pushSequence(1000))
-print("Part 2: %s"%pushSequence(0))
+
+########################### Part 2 ###########################
+check_dict = {}
+for dev in device_dict:
+    if "rx" in device_dict[dev].outs: check_dict[dev] = pushSequence(dev)
+#print("Part 2: %s"%pushSequence(0))
